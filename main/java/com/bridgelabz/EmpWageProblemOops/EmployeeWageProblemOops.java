36691371aecd181caae64a58af3cf2945f8ac82b
sonam1812 <sonampal832@gmail.com>
2021-09-01 15:22:01 +0530

    Added:Checked Wheather Employee is Present or Not

--- main/java/com/bridgelabz/EmpWageProblemOops/EmployeeWageProblemOops.java
+++ main/java/com/bridgelabz/EmpWageProblemOops/EmployeeWageProblemOops.java
@@ -1,8 +1,23 @@
 package com.bridgelabz.EmpWageProblemOops;
 
 public class EmployeeWageProblemOops {
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Employee Wage Computation");
+
+	 public void isPresent() {
+		
+		int isPresent=1;
+		int randomCheck = (int) (Math.floor(Math.random()*10)%2);
+		if(isPresent == randomCheck)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+		{
+			System.out.println("Employee is Abscent");
+		}
+	}
+	 public static void main(String[] args) {
+		EmployeeWageProblemOops obj = new EmployeeWageProblemOops();
+        obj.isPresent();
 	}
 
 }
