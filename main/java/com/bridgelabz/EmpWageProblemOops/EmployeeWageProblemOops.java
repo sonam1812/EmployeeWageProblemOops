01e5107af6afc5e4504656a4b7e88646a2df55c1
sonam1812 <sonampal832@gmail.com>
2021-09-01 15:08:13 +0530

    Welcome to Employee Wage Program

---
+++ main/java/com/bridgelabz/EmpWageProblemOops/EmployeeWageProblemOops.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.EmpWageProblemOops;
+
+public class EmployeeWageProblemOops {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Employee Wage Computation");
+	}
+
+}
