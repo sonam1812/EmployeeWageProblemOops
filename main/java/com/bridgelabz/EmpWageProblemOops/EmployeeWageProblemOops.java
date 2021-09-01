cb7d0dadf782c31969f990cc5266bf1b3f531126
sonam1812 <sonampal832@gmail.com>
2021-09-01 17:25:34 +0530

    Added:Calculate Salary

--- main/java/com/bridgelabz/EmpWageProblemOops/EmployeeWageProblemOops.java
+++ main/java/com/bridgelabz/EmpWageProblemOops/EmployeeWageProblemOops.java
@@ -3,36 +3,39 @@ package com.bridgelabz.EmpWageProblemOops;
 public class EmployeeWageProblemOops {
 	int wagePerHrs=20;
 	int dailyWage;
+	int workHrs=0;
+	int totalWage=0;
 		public static void main(String[] args) {
-		
+			for(int day=1;day<=20;day++) {
 			int randomCheck = (int) (Math.floor(Math.random()*10)%3);
 			System.out.println(randomCheck);
 			 EmployeeWageProblemOops obj=new EmployeeWageProblemOops();
 			 obj.wageCal(randomCheck);
-			
-			
+			}
 		}
+		
 		public void wageCal(int randomCheck) {
 			int dailyWage=0;
 			
-		switch(randomCheck) {
+			switch(randomCheck) {
 			   case 1:
 			   {
-					int halfTimeHrs=4;
-					dailyWage=wagePerHrs*halfTimeHrs;
-					System.out.println("DailyWage:"+dailyWage);
+				workHrs=4;
 					break;
 			   }
 			   case 2:
 			   {
-					int fullTimeHrs=8;
-					dailyWage=wagePerHrs*fullTimeHrs;
-					System.out.println("DailyWage:"+dailyWage);
+				   workHrs=8;	
 					break;
 			   }
-			   
+					default: {
+					
+						workHrs=0;
+					}
+			   }
+			dailyWage=wagePerHrs*workHrs;
+			System.out.println("DailySalary:"+dailyWage);
+			totalWage=totalWage+dailyWage;
+			System.out.println("Salary:"+totalWage);
 		}
 		}
-}
-	
-
