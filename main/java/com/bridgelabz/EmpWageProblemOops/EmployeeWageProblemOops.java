9e851344814ba011a7ac70e50e5f2a432e3019e6
sonam1812 <sonampal832@gmail.com>
2021-09-01 18:55:13 +0530

    Added: EmployeeWage Using Class and Method

--- main/java/com/bridgelabz/EmpWageProblemOops/EmployeeWageProblemOops.java
+++ main/java/com/bridgelabz/EmpWageProblemOops/EmployeeWageProblemOops.java
@@ -1,36 +1,37 @@
 package com.bridgelabz.EmpWageProblemOops;
 
 public class EmployeeWageProblemOops {
-	int wagePerHrs=20;
-	int workHrs = 0;
-	int totalWage=0;
-	int numWorkingDays=20;
-	int day=0;
-	int hrs=0;
-	int totalHrs=100;
+	public static final int wagePerHrs=20;
+	public static final int workHrs = 0;
+	public static final int isPartTime=1;
+	public static final int isFullTime=2;
+	public static final int numWorkingDays=20;
+	public static final int day=0;
+	public static final int hrs=0;
+	public static final int totalMonthlyHrs=10;
 	
 	public static void main(String[] args) {
+		int Salary=wageCal();
+		System.out.println("TotalWage:"+ Salary);
 			 EmployeeWageProblemOops obj=new EmployeeWageProblemOops();
 			 obj.wageCal();
-			
-		}
+	}
 		
-		public void wageCal() {
-			while(day<=numWorkingDays && hrs<= totalHrs)
-			{
+		public static int wageCal() {
+			int workHrs=0,hrs=0,day=0;
+			while(day<=numWorkingDays && hrs<=totalMonthlyHrs ) {
 				int randomCheck = (int) (Math.floor(Math.random()*10)%3);
 				day++;
 				
 				System.out.println("RandomCheckValue:"+randomCheck);
 			
-			
 			switch(randomCheck) {
-			 case 1:
+			 case isPartTime: 
 			   {
 					workHrs=4;
 					break;
 			   }
-			   case 2:
+			   case isFullTime: 
 			   {
 					workHrs=8;
 					break;
@@ -40,13 +41,11 @@ public class EmployeeWageProblemOops {
 				   workHrs=0; 
 				   break;
 			   }
-		}
+			}
 		hrs=hrs+workHrs;
 		System.out.println(hrs);
 	}
-	totalWage=wagePerHrs*hrs;
-	System.out.println("Salary:"+totalWage);
+	int totalWage=wagePerHrs*hrs;
+	return totalWage;
 }
 }
-	
-	
